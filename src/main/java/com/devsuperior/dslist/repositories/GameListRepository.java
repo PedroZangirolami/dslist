4567f57764be9d370cd99c01b866ae500cb4789f
PedroZangirolami <pedrozangirolami@gmail.com>
2023-07-11 21:41:16 -0300

    get lists

---
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslist.repositories;
+
+import com.devsuperior.dslist.entites.Game;
+import com.devsuperior.dslist.entites.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList,Long> {
+
+}
